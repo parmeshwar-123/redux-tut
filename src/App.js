9834865f63ec145784d768d86ff2dbf743ce8933
PARMESHWAR GHADGE <parmeshwarghadge07@gmail.com>
2022-05-01 22:17:36 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Redux tutorial with react.js</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur accusantium dolorem officiis placeat minima distinctio ad dignissimos odio porro ipsa nulla, et voluptas! Obcaecati est ea dicta neque. Repellendus, ea?</p>
+    </div>
+  );
+}
+
+export default App;
